3ad0e54fec24652ad46f86de78d6d38f918ff44c
Guillaume Lardillier <guillaume.lardillier@protonmail.com>
2018-12-01 18:39:03 +0100

    commit 0112

--- nov_server_reseausocial_1.0.js
+++ nov_server_reseausocial_1.0.js
@@ -24,8 +24,8 @@ const MongoClient = mongo.MongoClient;
 // const mongo = require('mongodb');
 
 // const MongoClient = mongo.MongoClient;
-//const url = "mongodb://localhost:27017/iambeezeedb";
-const url = "mongodb://admin:admin1@ds119374.mlab.com:19374/iambeezee"; 
+const url = "mongodb://localhost:27017/iambeezeedb";
+//const url = "mongodb://admin:admin1@ds119374.mlab.com:19374/iambeezee"; 
 
 //mongod.exe --dbpath=H:\MOUEDRAOGO\atelier_reseau_social\novembre_reseau_social\data
 
@@ -75,31 +75,56 @@ app.get('/', function (req, res) {
 
 let siteConnexionsNbr = 0;
 
-io.on('connexion', function (socket) { // qd un utilisateur (encore anonyme) arrive sur l url du site
+io.on('connect', function (socket) { // qd un utilisateur (encore anonyme) arrive sur l url du site
 
-    siteConnexionNbr++;
+    siteConnexionsNbr++;
     console.log("il y a eu " + siteConnexionsNbr + " connexions sur le site");
 
-    // socket.on('message1', function () {
-    //     console.log('le server a bien recu le message1')
-    //   });
+    socket.on('disconnect', function(){
+        siteConnexionsNbr--;
+        console.log("il y a eu " + siteConnexionsNbr + " connexions sur le site");
+    });
 
-    // socket.on('disconnect', function(){
-    //     console.log('member disconnected');
-    //   });
+    socket.on('newMembersNbr', function(data) { // nbre de nouveaux membres
+        console.log("new member");
+        console.log(data);
+    });
 
+    socket.on('connectedMembersNbr', function() { // nbre de membres connectés
+        console.log("connected new member");
+    });
+
+    //TO DO qd le user clique sur "validez" : creer un userID et stocker et matcher l email et la confirmation du mot de passe et le check des CGU(check true/false)  en bdd
+    //    socket.on('signUpInformations', function() {
+    //            console.log("kikou server");
+    //
+    //                }); // je renvoie, au server, les valeurs de signUpEmail, signUpPassword et de signUpConfirmPassword, afin de les stocker dans l'id du membre
+    //
+    //                    //TO DO stcoker ses data en userId et notamment sa date d'inscription
+    //                        //TO DO verifier si l utilisateur a une permission admin, si oui, afficher la div rs-navbar-admin
+    /*socket.on('membersValidateSignIn', function() {
+        if(){
+            socket.emit('ValidateSignInOk', ValidateSignInOk); // le mdp correpond bien a l email du membre
+        }else{
+            socket.emit('ValidateSignInNok', ValidateSignInNok); 
+
+        }
+    });*/
 }); // end of io.on
 
+//TO DO si validateSignIn == true, user === UserMember donc rafraichir ses data (Id + +date de connexion + permission + localisation + timeData + SearchData + friends dans rs-aside-friends-badge et rs-friends-connected-listGroup + Members dans rs-members-connected-listGroup + mon compte dans rs-asideAccount-listGroup ) dans son userID
+//    //TO DO si validateSignIn == false, envoyer un msg d avertissement
+//        //TO DO verifier si l utilisateur a une permission admin, si oui, afficher la div rs-navbar-admin
 /********************************************/
 
 // Ecoute serveur
 
 /********************************************/
 
-/*server.listen(8888, function () {
+server.listen(8888, function () {
+    console.log('CONNECTED TO local nov_server_reseausocial.js !');
+});
+/*jsserver.listen(process.env.PORT || 8888, function () {
     console.log('CONNECTED TO nov_server_reseausocial.js !');
 });*/
-server.listen(process.env.PORT || 8888, function () {
-    console.log('CONNECTED TO nov_server_reseausocial.js !');
-});
 // process.env.PORT = port de connexion pour l hebergement chez Heroku
--- public/nov_main_reseausocial_4.2.js
+++ public/nov_main_reseausocial_4.2.js
@@ -1,20 +1,27 @@
 //JQUERY
 (function ($) {
-    var socket;
     /*****************************
      *                            *
      *  AU CHARGEMENT DE LA PAGE  *
      *                            *
      *****************************/
 
-
+    console.log("ready");
+    debugger;
+    var socket = io();
 
     $(window).on("load", function () { // instruction lue mais exécutée une fois que toute la page est chargée
 
         // socket = io(); 
         // initialisation socket.io
 
-        var socket = io();
+        var test = document.getElementById('#includedNavbar');
+        console.log("LOL");
+        console.log(test);
+        debugger;
+        $("#includedNavbar").load("/html/nov_navbar_4.2.html", function(){ 
+            f_signUp();
+        });
 
         socket.emit('message1', function () {
             console.log('message1 envoyé par le client')
@@ -25,7 +32,6 @@
         // localisation par default = utilisation en parametre des coordonnees de la Tour Effeil
 
         /* SIGN UP */
-        // f_signUp();
 
         /* SIGN IN */
         // f_signIn();
@@ -44,14 +50,11 @@
      *                               *
      ********************************/
 
-    $(document).on("ready", function () { // execution immédiate dès que l'instruction est lue
+    /*$(document).on("ready", function () { // execution immédiate dès que l'instruction est lue
 
-        /* onOffSwitchButtonStatusChange */
-        // f_onOffSwitchButtonStatusChange ();
+        console.log("ready");
 
-
-
-    });
+    });*/
     // end of $(document).on("ready", function() {
 
 
@@ -61,13 +64,95 @@
 
     /*************************** INITIALISATION DES FONCTIONS ******************************************************************************/
 
+    function f_signUp() {
+        // TO DO affichage des CGU au click sur CGU , dans une popup avec dimensions specifiques
+        console.log("kikou");
+
+        let signupValidateBtn = $('.test');
+        let test = document.getElementById("#rs-signup-validate-btn");
+        debugger;
+
+        signupValidateBtn.click( function() {
+            console.log("kikou");
+            debugger;
+            let isFormValid = true; 
+            let signUpEmail = $("#SignUpEmail").get(0);
+            let signUpEmailValue = signUpEmail.value;
+
+            // verification de la correspondance des mots de passe lors de l'inscription
+            let signUpPassword = $("#SignUpPassword").get(0);
+            let signUpPasswordValue = signUpPassword.value;
+            if(!signUpPasswordValue || signUpPasswordValue == null) { 
+                isFormValid = false;
+            }
+
+            let signUpConfirmPassword = $("#SignUpConfirmPassword").get(0);
+            let signUpConfirmPasswordValue = signUpConfirmPassword.value;
+            if(!signUpConfirmPasswordValue || signUpConfirmPasswordValue == null) { 
+                isFormValid = false;
+            }
+
+            if(!isFormValid || signUpPasswordValue != signUpConfirmPasswordValue) {
+                signUpConfirmPassword.setCustomValidity("Votre confirmation de mot de passe ne correspond pas !");
+                console.log("confirmation nok");
+            } else {
+                console.log("confirmation ok");
+                let connexionStatus = "offline";
+                let newMembersNbr = 0;
+                let connectedMembersNbr = 0;
+
+                if (signUpConfirmPassword.setCustomValidity('')) {
+                    connexionStatus = "online"; // changement de statut                                                                                                    
+                    newMembersNbr++; // qd un nouveau membre s inscrit on incremente le nbre de nouveaux membres 
+                    connectedMembersNbr++; // qd un membre se connecte on incremente le nbre de membres
+                    console.log("Nombre de nouveaux membres = " + newMembersNbr);
+                    console.log("Nombre membres connectés = " + connectedMembersNbr);
+
+                };
+
+
+                socket.emit('newMembersNbr', newMembersNbr); // renvoi pour update coté server du nbre de nouveaux inscrits
+                socket.emit('connectedMembersNbr', connectedMembersNbr); // renvoi pour update coté server du nbre de membres connectés
+
+
+                let signupCguInput = $('#rs-signup-cgu-input')
+                let signupCguInputValue = signupCguInput.value
+
+                socket.emit('usersSignUpData',{signUpEmailValue, signUpPasswordValue, signUpConfirmPasswordValue, signupCguInputValue}); // renvoie, au server, les valeurs de signUpEmail, signUpPassword et de signUpConfirmPassword + acceptation des cgu, afin de les stocker dans l'id du membre
+
+            }
+        });
+
+
+
+        // TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO    TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO   TO DO
+
+
+        // TO DO POURQUOI MON URL N EST PAS RECONNUE ?
+        // function popUp('cgu_reseausocial_1.0.html') { 
+        //     newwindow=window.open(url,'PopUpCgu','height=500,width=600');
+        //     if (window.focus) {newwindow.focus()}
+        //     return false;
+        // }
+
+
+        // fermeture de la popup
+        // let cguCloseBtn = document.getElementsByClassName('rs-cgu-close-btn');
+
+        // document.getElementsByClassName('rs-signup-cgu-link').addEventListener('click', function() { 
+        //     cguMainContainer.style.display = "block"; 
+
+        //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             // }); 
+
+        //
+    }
+
+
 
 
 
 
 })(jQuery);
-
-
 
 
 
